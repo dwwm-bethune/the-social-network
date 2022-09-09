ebdac40e303d6d8541ac25bf67cfd35d2c1cb0d6
Matthieu Mota <matthieumota@gmail.com>
2022-09-09 12:50:36 +0200

    Create post

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Entity\User;
+use App\Form\PostType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,10 +25,24 @@ class UserController extends AbstractController
     }
 
     #[Route('/membre/{username}', name: 'app_user_show')]
-    public function show(User $user): Response
+    public function show(Request $request, User $user, EntityManagerInterface $manager): Response
     {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setCreator($this->getUser());
+
+            $manager->persist($post);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_user_show', ['username' => $this->getUser()->getUsername()]);
+        }
+
         return $this->render('user/show.html.twig', [
             'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 }
